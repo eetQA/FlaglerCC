c96ed65afb9c4a187a6c38062ee03d00ce63b4ff
agawhande <agawhande@e-emphasys.com>
2017-11-28 19:31:22 +0530

    28112017

---
+++ Object Repository/Service/ServiceOrder/ServiceOrderSegment/ServiceOrderSegment.OthersTab.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ServiceOrderSegment.OthersTab</name>
+   <tag></tag>
+   <elementGuidId>319c0680-ac44-4308-8dec-8f5c02ed1c1c</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>tssoc2110m002-1-pages-bar-page-2-label</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Service/ServiceOrder/ServiceOrderSegment/ServiceOrderSegment.Priority.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ServiceOrderSegment.Priority</name>
+   <tag></tag>
+   <elementGuidId>c572d05c-5f02-4d3e-9999-bcaf4e9a8738</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>tssoc2110m002-1-tssoc210.prpr-174-lookup-widget</value>
+   </webElementProperties>
+</WebElementEntity>
